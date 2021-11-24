8302b7ee7b07d78d51eda64a02742ab4f806350b
BBroomell1 <brandon.broomell1@gmail.com>
2021-11-24 10:53:56 -0500

    Display A List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'SomeDude'  },
+  { id: 12, name: 'SomeGuy'   },
+  { id: 13, name: 'ThisGuy'   },
+  { id: 14, name: 'ThatGuy'   },
+  { id: 15, name: 'ThisDude'  },
+  { id: 16, name: 'ThatDude'  },
+  { id: 17, name: 'Nobody'    },
+  { id: 18, name: 'Magma'     },
+  { id: 19, name: 'Tornado'   },
+  { id: 20, name: 'JohnDoe'   }
+];
